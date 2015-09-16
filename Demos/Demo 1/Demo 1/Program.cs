c047620c3b50878c96e9291c4fcad9418d0363e3
Chris Rankin <crankin2@studentmail.nait.ca>
2015-09-16 15:01:17 -0600

    Initialized

---
+++ Demos/Demo 1/Demo 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_1
+{
+    class Program
+    {
+        /********************************************
+        *
+        * Accepts one argument which must be a positive integer > 0
+        * Outputsspace delimited list of prime factors of arg 1
+        static void Main(string[] args)
+        {
+        }
+    }
+}
