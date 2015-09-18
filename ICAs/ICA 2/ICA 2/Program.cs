fe3128216e35623b09094100d00c57e797ebf2f1
Chris Rankin <crankin2@studentmail.nait.ca>
2015-09-18 11:07:58 -0600

    Error Message
    Created Error and Usage Messages

--- ICAs/ICA 2/ICA 2/Program.cs
+++ ICAs/ICA 2/ICA 2/Program.cs
@@ -11,5 +11,36 @@ namespace ICA_2
         static void Main(string[] args)
         {
         }
+        //General print error message
+        static void PrintError(string Err = "Unknown Failure", string Dbg = "",
+                                    bool printUsage = true, bool exit = false, int exitVal = 1)
+        {
+            //1 Print out error message
+            ConsoleColor currBackColour = Console.BackgroundColor;
+            ConsoleColor currForeColour = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Error.WriteLine("Error: " + Err);
+            if (Dbg.Length > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Error.WriteLine(Dbg);
+            }
+            Console.ForegroundColor = currForeColour;
+            Console.BackgroundColor = currBackColour;
+            if (printUsage)
+                PrintUsage();
+
+            if (exit)
+                Environment.Exit(exitVal);
+        }
+
+        //General usage message
+        static void PrintUsage()
+        {
+            Console.WriteLine("Usage: " + System.AppDomain.CurrentDomain.FriendlyName + " <val>" +
+                    " where <val> is a \npositive integer greater than zero. Prints \nevery arguement"
+                    " after the integer, repeating n number of times, where n is the integer");
+
+        }
     }
 }
