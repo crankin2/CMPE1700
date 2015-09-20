170f58283c60cef29e0eb5bab78dd627f2696267
Chris Rankin <crankin2@studentmail.nait.ca>
2015-09-20 15:52:13 -0600

    Error Messages and Main Program
    wrote error messages and finished main function of program

--- ICAs/ICA 2/ICA 2/Program.cs
+++ ICAs/ICA 2/ICA 2/Program.cs
@@ -10,6 +10,31 @@ namespace ICA_2
     {
         static void Main(string[] args)
         {
+            int tmp;
+            bool check;
+
+            if(args.Count() < 1) //if no arguments are passed
+            {
+                //print error and exit
+                PrintError("Invalid number of arguments (" + args.Count() + ")", "", true, true, -1);
+            }
+
+            check = int.TryParse(args[0], out tmp);//check if the first argument is an int
+
+            if (check == false)//if first argument isn't an int
+            {
+                //print error and exit
+                PrintError("Invalid first argument", "", true, true, -2);
+            }
+
+            for(int i = 0; i < tmp; i++)//repeat the loop tmp times, where tmp is the first argument
+            {
+                foreach (string arg in args.Skip(1))//write each element in the array, skipping the first
+                {
+                    Console.Write(arg + " ");
+                }
+                Console.WriteLine();
+            }         
         }
         //General print error message
         static void PrintError(string Err = "Unknown Failure", string Dbg = "",
@@ -30,6 +55,8 @@ namespace ICA_2
             if (printUsage)
                 PrintUsage();
 
+            Console.ReadKey();
+
             if (exit)
                 Environment.Exit(exitVal);
         }
@@ -38,9 +65,8 @@ namespace ICA_2
         static void PrintUsage()
         {
             Console.WriteLine("Usage: " + System.AppDomain.CurrentDomain.FriendlyName + " <val>" +
-                    " where <val> is a \npositive integer greater than zero. Prints \nevery arguement"
-                    " after the integer, repeating n number of times, where n is the integer");
-
+                    " where <val> is a \npositive integer greater than zero. Prints \nevery arguement" +
+                    " after the integer, repeating \nn number of times, where n is the integer");
         }
     }
 }
